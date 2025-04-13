edbfbc76cf914f5d29561abdc00ff6711f8c4383
pupupu014 <pupupu@pupupu-MacBook-Air.local>
2025-04-14 00:05:50 +0900

    private key 修正

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,13 +1,13 @@
 import admin from 'firebase-admin';
 
 
-console.log("🔥 FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY?.slice(0, 40));
+console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY?.slice(0, 40));
 
 const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
